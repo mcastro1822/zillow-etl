b4ca3c3516df435965e9673ceb77d93dce64c841
mcastro1822 <macastro1822@gmail.com>
2024-11-11 11:08:25 -0800

    Adds Type hints

--- src/zillow/transform/listing.py
+++ src/zillow/transform/listing.py
@@ -5,10 +5,18 @@ from zillow.property_model import Property
 
 
 @task(description="Converts Property Model to Dataframe")
-def property_json_to_df(property_json: dict):
-    """ """
+def property_json_to_df(property_json: dict) -> pl.DataFrame:
+    """
+    Converts incoming property json to polars dataframe
+
+    Args:
+        property_json: json of property key value pairs
+
+    Returns:
+        df
+    """
     property = Property.model_validate(property_json.get("property")).model_dump()
 
     df: pl.DataFrame = pl.from_dict(property).unnest("resoFacts").unnest("address")
-    # df.write_parquet('property.parquet', compression='snappy')
+
     return df
